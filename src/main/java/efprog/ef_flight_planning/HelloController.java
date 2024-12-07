6264d05fc546ae14d3b2422dcc1947c034234d33
eckofox1981 <franklineck@pm.me>
2024-12-07 22:28:43 +0100

    first commit

---
+++ src/main/java/efprog/ef_flight_planning/HelloController.java
@@ -0,0 +1,14 @@
+package efprog.ef_flight_planning;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
